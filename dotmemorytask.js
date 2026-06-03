7b8c2a188638fc9692eb68e6b0150ff7bea52cd1
KeyboardCarnage <77650827+KeyboardCarnage@users.noreply.github.com>
2026-06-03 19:14:39 +0200

    Update dotmemorytask.js

--- dotmemorytask.js
+++ dotmemorytask.js
@@ -1,6 +1,6 @@
 // Dot Memory and Sentence Judgement Dual Task 
 // Based on De Neys & Schaeken (2007), Dieussaert et al. (2011) and Marty & Chemla (2013)
-// This code uses jsPsych to implement a dot pattern memory task with sentence judgement
+// This code uses jsPsych to implement a dot pattern memory task and sentence judgement dual-task
 
 // Hide next button
 var qthis = this;
